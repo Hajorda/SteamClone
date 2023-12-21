005876e137faa84564307b6084d96a62ae2a1fe1
HajordaM <hajorda@gmail.com>
2023-12-22 00:01:05 +0300

    Bugfixes

--- src/main/java/com/balikicindebalik/steamclone/HelloApplication.java
+++ src/main/java/com/balikicindebalik/steamclone/HelloApplication.java
@@ -11,7 +11,7 @@ import java.net.URL;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(new URL("file:src/main/java/com/balikicindebalik/steamclone/fxml/login/loginV01.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(new URL("file:src/main/java/com/balikicindebalik/steamclone/ui/loginV01.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
