c0b36c298cb57d9aea201976cb7113d2b2d1e9dc
kutayzku <kutayzku@gmail.com>
2023-12-22 00:35:05 +0300

    a

--- src/main/java/com/balikicindebalik/steamclone/controller/LoginController.java
+++ src/main/java/com/balikicindebalik/steamclone/controller/LoginController.java
@@ -1,4 +1,24 @@
 package com.balikicindebalik.steamclone.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private TextField Password;
+
+    @FXML
+    private TextField User;
+
+    @FXML
+    void LoginAction(ActionEvent event) {
+
+    }
+
 }
